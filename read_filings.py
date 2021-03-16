32cb6466a8ecc07dffc893927236a91901c0d4ba
Matt <lynma01@gmail.com>
2021-03-16 13:24:00 -0400

    retool to use py-edgar library

--- read_filings.py
+++
@@ -1,6 +0,0 @@
-from beautifulsoup4 import BeautifulSoup
-
-with open("./sec-edgar-filings/AAPL/8-K/0001104659-15-019336/filing-details.html", "r") as f:
-    read_data = BeautifulSoup(f, 'html.parser')
-
-    print(read_data.getText())
