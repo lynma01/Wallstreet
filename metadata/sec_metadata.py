98d9189a160adf7f7b591977fb733e1567f76c4d
Matt <lynma01@gmail.com>
2021-03-17 15:45:26 -0400

    output metadata to list of dicts

---
+++ metadata/sec_metadata.py
@@ -0,0 +1,24 @@
+from edgar import Company
+from collections import ChainMap
+import json
+
+def return_filings(doc_to_append, appendee):
+    doc_to_append = doc_to_append.__dict__
+    del doc_to_append['element']
+    appendee.append(doc_to_append)
+
+def get_filing_metadata(name, cik, filing, no_filings) -> list:
+    company = Company(name, cik)
+    tree = company.get_all_filings(filing_type = filing)
+    docs = Company.get_documents(tree, no_of_documents=no_filings, as_documents=True)
+
+    filings = []
+
+    if no_filings == 1:
+        return_filings(docs, filings)
+    else:
+        for document in docs:
+            return_filings(document, filings)
+    return(filings)
+
+print(get_filing_metadata("Oracle Corp", "0001341439", "10-K", 3))
