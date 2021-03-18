d38da8d0b6a3856bfd6bbeaa5a177bf2704b08c9
Matt <lynma01@gmail.com>
2021-03-18 11:45:02 -0400

    optimize class with yield method

--- metadata/sec_metadata.py
+++ metadata/sec_metadata.py
@@ -3,31 +3,26 @@ from collections import ChainMap
 import json
 
 class SecMeta:
-
     def __init__(self):
         pass
 
     @classmethod
-    def return_filings(cls, doc_to_append, appendee):
+    def clean_filings(cls, doc_to_append, cik):
         doc_to_append = doc_to_append.__dict__
         del doc_to_append['element']
-        appendee.append(doc_to_append)
-
+        del doc_to_append['content']['Documents']
+        doc_to_append['cik'] = cik
+        return doc_to_append
     
     def get_filing_metadata(self, name, cik, filing, no_filings) -> list:
         company = Company(name, cik)
         tree = company.get_all_filings(filing_type = filing)
         docs = Company.get_documents(tree, no_of_documents= no_filings, as_documents=True)
-
-        filings = []
-
         if no_filings == 1:
-            return_filings(docs, filings)
+            return self.clean_filings(docs, cik)
         else:
             for document in docs:
-                self.return_filings(document, filings)
-        
-        return(filings)
+                yield self.clean_filings(document, cik)
 
 
-print(SecMeta().get_filing_metadata(name="Oracle Corp", cik="0001341439", filing="10-K", no_filings=5))
+print((list(SecMeta().get_filing_metadata(name="Oracle Corp", cik="0001341439", filing="10-K", no_filings=5))))
