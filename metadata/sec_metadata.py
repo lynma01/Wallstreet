081f45539463da5f829fc36f0c6f01ebf432935d
Matt <lynma01@gmail.com>
2021-03-18 10:20:05 -0400

    reorganize metadata methods into class

--- metadata/sec_metadata.py
+++ metadata/sec_metadata.py
@@ -2,23 +2,32 @@ from edgar import Company
 from collections import ChainMap
 import json
 
-def return_filings(doc_to_append, appendee):
-    doc_to_append = doc_to_append.__dict__
-    del doc_to_append['element']
-    appendee.append(doc_to_append)
+class SecMeta:
 
-def get_filing_metadata(name, cik, filing, no_filings) -> list:
-    company = Company(name, cik)
-    tree = company.get_all_filings(filing_type = filing)
-    docs = Company.get_documents(tree, no_of_documents=no_filings, as_documents=True)
+    def __init__(self):
+        pass
 
-    filings = []
+    @classmethod
+    def return_filings(cls, doc_to_append, appendee):
+        doc_to_append = doc_to_append.__dict__
+        del doc_to_append['element']
+        appendee.append(doc_to_append)
 
-    if no_filings == 1:
-        return_filings(docs, filings)
-    else:
-        for document in docs:
-            return_filings(document, filings)
-    return(filings)
+    
+    def get_filing_metadata(self, name, cik, filing, no_filings) -> list:
+        company = Company(name, cik)
+        tree = company.get_all_filings(filing_type = filing)
+        docs = Company.get_documents(tree, no_of_documents= no_filings, as_documents=True)
 
-print(get_filing_metadata("Oracle Corp", "0001341439", "10-K", 3))
+        filings = []
+
+        if no_filings == 1:
+            return_filings(docs, filings)
+        else:
+            for document in docs:
+                self.return_filings(document, filings)
+        
+        return(filings)
+
+
+print(SecMeta().get_filing_metadata(name="Oracle Corp", cik="0001341439", filing="10-K", no_filings=5))
