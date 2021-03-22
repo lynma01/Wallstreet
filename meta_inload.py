cea2bbc85f1a01b88c04a2c0d1b7284edf3a8216
Matt <lynma01@gmail.com>
2021-03-22 11:52:53 -0400

    add __main__ to meta_inload

--- meta_inload.py
+++ meta_inload.py
@@ -1,4 +1,7 @@
 import src.metadata.sec_metadata as md
 
-print(list(md.SecMeta().get_filing_metadata(name="Oracle Corp", cik="0001341439", filing="10-K", no_filings=5)))
+if __name__ == "__main__":
+    list(md.SecMeta().get_filing_metadata(name="Oracle Corp", cik="0001341439", filing="10-K", no_filings=5))
+
+    print('metadata inload complete!')
 
