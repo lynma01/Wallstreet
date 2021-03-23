bf7a9eb12a63cc5378aeb1b8fe21e81a7094a6fe
Matt <lynma01@gmail.com>
2021-03-23 14:28:31 -0400

    move SecMeta() into dagster syntax in /src/pipes Fixes #7

--- meta_inload.py
+++
@@ -1,7 +0,0 @@
-import src.metadata.sec_metadata as md
-
-if __name__ == "__main__":
-    list(md.SecMeta().get_filing_metadata(name="Oracle Corp", cik="0001341439", filing="10-K", no_filings=5))
-
-    print('metadata inload complete!')
-
