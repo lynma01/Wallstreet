cabc556c5f68a4dfff867209d9d3b91441dabbf1
Matt <lynma01@gmail.com>
2021-03-23 14:31:29 -0400

    create issue for clean_filings()

--- pipes/metadata.py
+++ pipes/metadata.py
@@ -5,14 +5,13 @@ from dagster import (
     solid, pipeline, Field, OutputDefinition, InputDefinition, Output
 )
 
-# TODO: #7 move SecMeta() into dagster syntax in /src/pipes
-
 @solid
 def get_filing_metadata(context, name: str, cik: str, filing: str, no_filings: int):
     company = Company(name, cik)
     tree = company.get_all_filings(filing_type = filing)
     docs = Company.get_documents(tree, no_of_documents= no_filings, as_documents=True)
 
+    # TODO #13 move clean_filings() into its own solid def
     def clean_filings(doc_apnd: Dict, cik: str, filing: str):
         doc_apnd = doc_apnd.__dict__
         doc_apnd.pop('element')
