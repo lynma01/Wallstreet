3ec23424b9b466e8c28b0bd660c8548c369f0c34
Matt <lynma01@gmail.com>
2021-03-26 13:41:52 -0400

    move clean_filings() into its own solid def Fixes #13

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -1,16 +1,40 @@
-import pytest
 import time
 from pipes import metadata as md
-
 from dagster import (
-    pipeline, solid, execute_pipeline, execute_solid,
-    SolidExecutionResult, PipelineExecutionResult
+    execute_pipeline,
+    execute_solid,
+    PipelineExecutionResult,
+    SolidExecutionResult,
+    check_dagster_type,
+    Dict,
+    List,
+    Any
 )
 
-
-def test1():
+def test_meta_solid():
     start_time = time.time()
+    run_config={
+                "solids": {
+                    "get_filing_metadata": {
+                        "inputs": {
+                                "name":"Oracle Corp", 
+                                "cik": "0001341439", 
+                                "filing": "10-K", 
+                                "no_filings": 5
+                            }
+                        }   
+                    }
+                }
+    
+    result = execute_solid(md.get_filing_metadata, run_config=run_config)
+    assert isinstance(result, SolidExecutionResult)
+    assert result.success
+    assert check_dagster_type(List[Dict[Any, Any]], [{"foo":"bar"}, {"foo":"bar"}]).success
 
+    print("--- %s Seconds ---" % (time.time() - start_time))
+
+def test_meta_pipeline():
+    start_time = time.time()
     run_config={
             "solids": {
                 "get_filing_metadata": {
@@ -29,3 +53,4 @@ def test1():
     assert result.success
 
     print("--- %s Seconds ---" % (time.time() - start_time))
+
